42c9e7243829344ce9f34fa9103a6d56f6705660
pacse <68709871+pacse@users.noreply.github.com>
2025-05-07 10:51:17 +1200

    Update Ver1Delta.py
    Final version of Delta. Enigma class is functional, but overcomplicated and probably not historically accurate.

--- Ver1Delta.py
+++ Ver1Delta.py
@@ -1,4 +1,5 @@
-# PEM Version Delta – WIP
+# PEM Version Delta – Works
+import copy
 import secrets
 from dataclasses import dataclass
 from typing import Tuple, List, TypeAlias
@@ -9,6 +10,9 @@ ModResults: TypeAlias = List[List[int]]
 class EncryptedMessage:
   message: str
   mod_results: ModResults
+
+ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+A = ord("A")
 
 #message: str = "Hello. I like rice."
 #ciphertext: str = ""
@@ -100,19 +104,97 @@ def srandrotorwiring() -> list[int]:
 
   return wiring
 
+# class for Enigma Cipher
+def check_type(variable, check_types: list[type]) -> bool:
+  '''
+  Checks if a variable is equal to a type with a list of types
+  eg. to check for list[list[int]] check_type = [list, list, int]
+  '''
+  # no types specified
+  if not check_types:
+    return True
+
+  # just check the first type
+  check = check_types[0]
+  if not isinstance(variable, check):
+    return False
+  # variable is equal to check type
+
+  if check != list: # no more checking needed
+    return True
+
+  if len(check_types) > 1:
+    # recursively check variable for lists of lists ect
+    return all(check_type(item, check_types[1:]) for item in variable)
+
+  # final fallback
+  return True # hasn't triggered anything else, could just be check_type == list
+
+def char_to_ascii(char: str) -> int:
+  '''
+  converts a character to it's ASCII value
+  '''
+  try:
+    return ord(char) - ASCII_MIN
+  except Exception as e:
+    print(f"Error: {e}")
+
+def ascii_to_char(char: int) -> str:
+  '''
+  converts an ASCII value to it's character
+  '''
+  return chr(char + ASCII_MIN)
+
+def alpha_to_index(char: str) -> int:
+  '''
+  converts a capital character to it's alphabetical index
+  '''
+  if len(char) != 1:
+    raise Exception(f"Char: {char!r} is longer than 1")
+
+  return ord(char.upper()) - A + 1
+
+def ascii_to_index(char: int) -> str:
+  '''
+  converts an alphabetical index to it's capital character
+  '''
+  if char > 26:
+    raise Exception(f"Char: {char!r} is greater than 25 (Z)")
+
+  return chr(char + A - 1)
+
+def srandrotorwiring() -> list[int]:
+  '''
+  Securely generate random rotor wiring
+  returning a shuffled ascii list
+
+  choices = [i for i in range(ASCII_RANGE)]
+  wiring: list[int] = []
+  '''
+  choices = [i for i in range(len(ALPHABET))]
+  wiring: list[int] = []
+
+  while len(choices) > 0:
+    index = secrets.randbelow(len(choices))
+    wiring.append(choices[index])
+    choices.pop(index)
+
+  return wiring
+
 # class for Ascii Enigma Cipher
 class Enigma:
   class Keyboard:
 
     def forward(self, letter: str) -> int: # pass from input
-      return char_to_ascii(letter) # signal
+      return ord(letter) - ord("A") # signal
 
     def backward(self, signal: int) -> str: # pass to output
-      return chr(signal + ASCII_MIN) # letter
+      return chr(signal + ord("A")) # letter
 
   class Plugboard:
 
     def __init__(self, plugs: list[str]) -> None:
+      plugs = plugs if plugs != None else []
       # list for wiring
       self.wiring: list[int] = [i for i in range(ASCII_RANGE)]
 
@@ -131,40 +213,96 @@ class Enigma:
 
   class Rotor:
 
-    def __init__(self, wiring: list[int] or str, notch: int = 0) -> None:
+    def __init__(self,
+                 wiring: list[int] or str,
+                 notch: list[int] or str or None = None) -> None:
+
+      # current position in "window"
+      self.position = 0
+
       # list for wiring
       if check_type(wiring, [list, int]):
         self.wiring: list[int] = wiring
 
       elif check_type(wiring, [str]):
-        self.wiring: list[int] = [char_to_ascii(wire) for wire in wiring]
-      
-      else:
-        raise Exception(f"Could not initialise wiring with {wiring}")
+        self.wiring: list[int] = [ord(wire) - A for wire in wiring]
 
-    def forward_and_backward(self, signal: int) -> int:
+      else:
+        raise Exception(f"Could not initialise wiring with {wiring!r}")
+
+      self.reverse_wiring = [0] * len(self.wiring)
+      for index, value in enumerate(self.wiring):
+        self.reverse_wiring[value] = index
+
+      # === Inititalize self.notch ===
+      if notch is None:
+        self.notch = None
+
+      elif check_type(notch, [str]):
+        self.notch = [alpha_to_index(letter) - 1 for letter in notch]
+
+      elif check_type(notch, [list, int]):
+        self.notch = notch
+
+      else:
+        raise Exception(f"Notch type invalid with {notch!r}")
+
+    def forward(self, signal: int) -> int:
       return self.wiring[signal]
 
+    def backward(self, signal: int) -> int:
+      return self.reverse_wiring[signal]
+
+    def rotate(self, n = 1, is_forward = True) -> None:
+      # rotate wiring
+      for _ in range(n):
+        if is_forward:
+          self.wiring = self.wiring[1:] + [self.wiring[0]]
+          self.position = (self.position + 1) % 26
+        else:
+          self.wiring = [self.wiring[-1]] + self.wiring[:-1]
+          self.position = (self.position - 1) % 26
+
+      # update reverse wiring
+      self.reverse_wiring = [0] * len(self.wiring)
+      for index, value in enumerate(self.wiring):
+        self.reverse_wiring[value] = index
+
+    def set_ring(self, position: int) -> None:
+      self.rotate(position-1, is_forward=False)
+
+      # find where the notch is
+      if self.notch is not None:
+        new_notch_positions = []
+        for notch in self.notch:
+          new_notch_positions.append(self.wiring.index(notch))
+        self.notch = new_notch_positions
+
+    def rotate_to_letter(self, letter):
+      index = ord(letter.upper()) - A
+      rotations = (index - self.position) % 26
+      self.rotate(rotations)
+
   class Reflector:
-    def __init__(self, wiring: list[int]) -> None:
+    def __init__(self, wiring: list[int] or str) -> None:
         # list for wiring
       if check_type(wiring, [list, int]):
         self.wiring: list[int] = wiring
 
       elif check_type(wiring, [str]):
-        self.wiring: list[int] = [char_to_ascii(wire) for wire in wiring]
+        self.wiring: list[int] = [ord(wire) - A for wire in wiring]
 
     def reflect(self, signal: int) -> int:
       return self.wiring[signal]
 
   hist_rotors: dict[str, Rotor] = { # historical settings from wikipedia
 
-    # Commercial Enigma A, B
+    # Commercial Enigma A, B (stepped together, no notches)
     "IC": Rotor("DMTWSILRUYQNKFEJCAZBPGXOHV"),
     "IIC": Rotor("HQZGPJTMOBLNCIFDYAWVEUSRKX"),
     "IIIC": Rotor("UQNTLSZFMREHDPXKIBVYGJCWOA"),
 
-    # Swiss K
+    # Swiss K (stepped together, no notches)
     "I-K": Rotor("PEZUOHXSCVFMTBGLRINQJWAYDK"),
     "II-K": Rotor("ZOUESYDKFWPCIQXHMVBLGNJRAT"),
     "III-K": Rotor("EHRVXGAOBQUSIMZFLYNWKTPDJC"),
@@ -172,18 +310,18 @@ class Enigma:
     "ETW-K": Rotor("QWERTZUIOASDFGHJKPYXCVBNML"),
 
     # Enigma I
-    "I": Rotor("EKMFLGDQVZNTOWYHXUSPAIBRCJ"),
-    "II": Rotor("AJDKSIRUXBLHWTMCQGZNPYFVOE"),
-    "III": Rotor("BDFHJLCPRTXVZNYEIWGAKMUSQO"),
+    "I": Rotor("EKMFLGDQVZNTOWYHXUSPAIBRCJ", "Q"),
+    "II": Rotor("AJDKSIRUXBLHWTMCQGZNPYFVOE", "E"),
+    "III": Rotor("BDFHJLCPRTXVZNYEIWGAKMUSQO", "V"),
 
     # M3 Army
-    "IV": Rotor("ESOVPZJAYQUIRHXLNFTGKDCMWB"),
-    "V": Rotor("VZBRGITYUPSDNHLXAWMJQOFECK"),
+    "IV": Rotor("ESOVPZJAYQUIRHXLNFTGKDCMWB", "J"),
+    "V": Rotor("VZBRGITYUPSDNHLXAWMJQOFECK", "Z"),
 
     # M3 & M4 Naval
-    "VI": Rotor("JPGVOUMFYQBENHZRDKASXLICTW"),
-    "VII": Rotor("NZJHGRCXMYSWBOUFAIVLPEKQDT"),
-    "VIII": Rotor("FKQHTLXOCBJSPDZRAMEWNIUYGV"),
+    "VI": Rotor("JPGVOUMFYQBENHZRDKASXLICTW", "ZM"),
+    "VII": Rotor("NZJHGRCXMYSWBOUFAIVLPEKQDT", "ZM"),
+    "VIII": Rotor("FKQHTLXOCBJSPDZRAMEWNIUYGV", "ZM"),
   }
 
   hist_reflectors: dict[str, Reflector] = { # historical settings from wikipedia
@@ -192,10 +330,6 @@ class Enigma:
     "A": Reflector("EJMZALYXVBWFCRQUONTSPIKHGD"),
     "B": Reflector("YRUHQSLDPXNGOKMIEBFZCWVJAT"),
     "C": Reflector("FVPJIAOYEDRZXWGCTKUQSBNMHL"),
-
-    # M4 R2
-    "Beta": Reflector("LEYJVCNIXWPBQMDRTAKZGFUHOS"),
-    "Gamma": Reflector("FSOKANUERHMBTIYCWLQPZXVGJD"),
 
     # M4 R1 (M3 + Thin)
     "B Thin": Reflector("ENKQAUYWJICOPBLMDXZVFTHRGS"),
@@ -206,75 +340,178 @@ class Enigma:
   }
 
   def __init__(self,
-               use_hist_rotors: bool,
+               hist_rotors: bool,
                rotors: list[str] or list[list[int]],
-               use_hist_reflectors: bool,
+               start_positions: str,
+               hist_reflectors: bool,
                reflector: list[int] or str,
-               plugboard: list[str] or None) -> None:
+               plugboard: list[str] or None,
+               key: str) -> None:
 
     # === initialize rotors ===
 
-    # historical settings using key: value pairs
-    if use_hist_rotors and check_type(rotors, [list, str]):
-      self.rotors: list[Enigma.Rotor] = []
-      for rotor in rotors:
-        self.rotors.append(Enigma.hist_rotors[rotor])
+    # validate start positions
+    if len(start_positions) != len(rotors):
+      raise Exception(f"Not enough start positions for rotors {start_positions!r}\nExpected {len(rotors)}, got {len(start_positions)}")
 
-    # custom settings as a list list of ints for signals
-    elif check_type(rotors, [list, list, int]):
-      self.rotors: list[Enigma.Rotor] = []
+    # historical settings using dictionary
+    if hist_rotors and check_type(rotors, [list, str]):
+      self.rotors: list[Enigma.Rotor] = [] # init rotor list
       for rotor in rotors:
-        self.rotors.append(Enigma.Rotor(rotor))
+        self.rotors.append(copy.deepcopy(Enigma.hist_rotors[rotor])) # deepcopy for a new instance
 
-    # Rotor also accepts a list of strs for signals
-    elif check_type(rotors, [list, str]):
-      self.rotors: list[Enigma.Rotor] = []
+    # custom settings as list[list[int]] for signals
+    elif not hist_rotors and check_type(rotors, [list, list, int]):
+      self.rotors: list[Enigma.Rotor] = [] # init rotor list
       for rotor in rotors:
-        self.rotors.append(Enigma.Rotor(rotor))
+        self.rotors.append(Enigma.Rotor(rotor)) # append rotor
+
+    # Rotor also accepts list[str] for signals
+    elif not hist_rotors and check_type(rotors, [list, str]):
+      self.rotors: list[Enigma.Rotor] = [] # init rotor list
+      for rotor in rotors:
+        self.rotors.append(Enigma.Rotor(rotor)) # append rotor
 
     # invalid
     else:
-      raise Exception(f"Invalid rotor assignment: {rotors}\nExpected type list[list[int]] or list[str], got {type(rotors)}")
+      raise Exception(f"Invalid rotor assignment with {rotors!r}\nExpected type list[list[int]] or list[str]")
+
+    # store initial rotor positions
+    self.initial_positions = [alpha_to_index(pos) - 1 for pos in start_positions]
+
+    # store initial wirings for reset function
+    self.initial_wirings = []
+    self.initial_reverse_wirings = []
+    for rotor in self.rotors:
+      self.initial_wirings.append(rotor.wiring)
+      self.initial_reverse_wirings.append(rotor.reverse_wiring)
+
+    # rotate to start positions
+    for i, rotor in enumerate(self.rotors):
+      rotor.rotate_to_letter(start_positions[i])
 
     # === initialise reflector ===
-    
+
     # historical settings
-    if use_hist_reflectors and check_type(reflector, [str]):
-      self.reflector: Enigma.Reflector = Enigma.hist_reflectors[reflector]
+    if hist_reflectors and check_type(reflector, [str]):
+      self.reflector: Enigma.Reflector = copy.deepcopy(Enigma.hist_reflectors[reflector]) # deepcopy for a new instance
 
     # custom settings
-    elif not use_hist_reflectors and (check_type(reflector, [str]) or check_type(reflector, [list, int])):
+    elif not hist_reflectors and (check_type(reflector, [str]) # Allowed types:
+    or check_type(reflector, [list, int])):                    # str, list[int]
       self.reflector: Enigma.Reflector = Enigma.Reflector(reflector)
 
+    # invalid
     else:
-      raise Exception(f"Invalid reflector assignment: {rotors}\nExpected type str or list[int], got {type(rotors)}")
+      raise Exception(f"Invalid reflector assignment: {reflector!r}\nExpected type str or list[int]")
 
     # === initialise plugboard ===
-    if check_type(plugboard, [list, str]) or plugboard == None:
+    if check_type(plugboard, [list, str]) or plugboard == None: # Accept types list[str] or None
       self.plugboard: Enigma.Plugboard = Enigma.Plugboard(plugboard)
-        
+
+    # invalid
     else:
-      raise Exception(f"Invalid plugboard assignment: {plugboard}\nExcepted type list[str] or None, got {type(plugboard)}")
+      raise Exception(f"Invalid plugboard assignment: {plugboard!r}\nExcepted type list[str] or None")
 
     # === initialize keyboard ===
     self.keyboard = Enigma.Keyboard()
 
-  def pass_message(self, message: str):
+  def reset(self) -> None:
+    '''
+    resets rotors to starting positions
+    '''
+    for i, rotor in enumerate(self.rotors):
+      rotor.position = 0 # reset position
+      rotor.wiring = copy.deepcopy(self.initial_wirings[i]) # set wiring
+      rotor.reverse_wiring = copy.deepcopy(self.initial_reverse_wirings[i]) # & reverse wiring
+      rotor.rotate(self.initial_positions[i]) # rotate to init pos
+
+  def set_key(self, key: str) -> None:
+    '''
+    sets rotors to begin with key
+    '''
+    # ensure we have enough keys (sanity check)
+    if len(key) != len(self.rotors):
+      raise Exception(f"Key length ({len(key)}) does not equal number of rotors ({len(self.rotors)})")
+
+    for i, letter in enumerate(key):
+      # i is key index, letter is letter at index
+      self.rotors[i].rotate_to_letter(letter)
+
+  def set_rings(self, rings: str):
+    '''
+    moves the notch of a rotor through rotor shifting
+    '''
+    # ensure we have enough rings (sanity check)
+    if len(rings) != len(self.rotors):
+      raise Exception(f"provided rings ({rings!r}, length: {len(rings)}) does not equal number of rotors ({len(self.rotors)})")
+
+    for i, ring in enumerate(rings):
+      # i is ring index, ring is ring at index
+      position = alpha_to_index(ring)
+      self.rotors[i].set_ring(position)
+
+  def step_rotors(self, n = 1) -> None:
+    '''
+    step rotors n times, cascading if applicable
+    '''
+    for _ in range(n):
+      rotor_length = len(self.rotors)
+
+      # list of rotors that should step
+      do_step = [False] * rotor_length
+
+      # rightmost rotor always steps
+      do_step[-1] = True
+
+      # traverse do_step right to left
+      # checking which rotors should step.
+      # if rtr i is stepping and is at
+      # notch, rtr i-1 steps too.
+      for i in range(rotor_length-1, 0, -1):
+        if (do_step[i] and self.rotors[i].notch) and self.rotors[i].position in self.rotors[i].notch: # at notch and stepping
+          do_step[i-1] = True
+
+      #print(f"[DEBUG] Rotor Stepping Flags: {do_step}")  # << Add this
+
+      # apply rotor stepping
+      for index, rotor in enumerate(self.rotors):
+        if do_step[index]:
+          rotor.rotate()
+
+  def pass_message(self, message: str, key: str):
     '''
     passes a provided message through the enigma
     '''
     result: str = ""
-    
-    for char in plaintext:
-      signal = self.keyboard.forward(char)                 # keyboard first
-      signal = self.plugboard.forward_and_backward(signal) # then plugboard
-      for rotor in self.rotors:                           
-        signal = rotor.forward_and_backward(signal)        # then rotors
-      signal = self.reflector.reflect(signal)              # reflector
-      for rotor in self.rotors[::-1]:                           
-        signal = rotor.forward_and_backward(signal)        # rotors again (reverse order)
-      signal = self.plugboard.forward_and_backward(signal) # plugboard again
-      result += self.keyboard.backward(signal)             # finally append to result
+
+    # position rotors for message key
+    self.set_key(key)
+
+    for char in message:
+      char = char.upper()
+      #print(f"[DEBUG] Input Char: {char}, Rotor Positions Before Step: {[r.position for r in self.rotors]}")
+      # step rotors
+      self.step_rotors()
+      #print(f"[DEBUG] Rotor Positions After Step: {[r.position for r in self.rotors]}")
+      if char not in ALPHABET:
+        result += char
+
+      else:
+        signal = self.keyboard.forward(char)                 # keyboard first
+        signal = self.plugboard.forward_and_backward(signal) # then plugboard
+
+        for rotor in self.rotors[::-1]:                      # left to right rotors
+          signal = rotor.forward(signal)
+
+        signal = self.reflector.reflect(signal)              # reflector
+
+        for rotor in self.rotors:                            # rotors again (right to left)
+          signal = rotor.backward(signal)
+        signal = self.plugboard.forward_and_backward(signal) # plugboard again
+        result += self.keyboard.backward(signal)             # finally append to result
+
+    return result
 
 def gen_random_strs(amount: int, length: int) -> list[str]:
   '''
@@ -287,6 +524,9 @@ def gen_random_strs(amount: int, length: int) -> list[str]:
   return random_strs
 
 def populate_mod_list(length: int) -> ModResults:
+  '''
+  depricated
+  '''
   return [[0, 0] for i in range(length)]
 
 def get_key_letters(keys: list[str], index: int) -> list[int]:
@@ -297,6 +537,9 @@ def get_key_letters(keys: list[str], index: int) -> list[int]:
   return [char_to_ascii(keys[i][index]) for i in range(len(keys))]
 
 def do_math(keys: list[str], message: str, mod_results: ModResults) -> EncryptedMessage:
+  '''
+
+  '''
   result = ""
 
   # for each character
@@ -393,10 +636,11 @@ if __name__ == "__main__":
   rotors = ["I", "II", "III", "IV", "V"]
   reflector = "A"
   plugboard = ["AF", "CR", "EQ"]
-  machine = Enigma(True, rotors, True, reflector, plugboard)
+  key = "A" * 5
+  machine = Enigma(True, rotors, key, True, reflector, plugboard, key)
   plaintext = "Hello. I like rice."
-  encrypted_text = machine.pass_message(plaintext)
-  decrypted_text = machine.pass_message(plaintext)
+  encrypted_text = machine.pass_message(plaintext, key)
+  decrypted_text = machine.pass_message(encrypted_text, key)
 
   print(f"Plain Text: {plaintext}")
   print(f"Encrypted Text: {encrypted_text}")
